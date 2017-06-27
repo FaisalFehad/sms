dcd6ebd284c7d7df2edd059af5dcd6ba17994365
faisalfehad <faisalfehad@gmail.com>
2017-06-27 11:31:46 +0100

    Dynamic page titles

--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class StocksControllerTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @base_title = "Stationery Management System"
+  end
+
   test "should get home" do
     get root_path
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 end
