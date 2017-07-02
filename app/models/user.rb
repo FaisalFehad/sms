c8966d324e4608014318b297a8539a9fa148d2d6
faisalfehad <faisalfehad@gmail.com>
2017-07-02 13:18:56 +0100

    prepare User model for disabling accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ApplicationRecord
   has_many :orders
   has_many :stocks, through: :orders
 
+  # https://stackoverflow.com/questions/6004216/devise-how-do-i-forbid-certain-users-from-signing-in
+    def active_for_authentication?
+    #remember to call the super
+    #then put our own check to determine "active" state using
+    #our own "is_active" column
+    super and self.is_active?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'user accounts should be active by default' do
+    active = users(:my_user)
+    assert active.is_active?
+  end
+
+  test 'active_for_authentication? method should return true' do
+    active = users(:my_user)
+    assert active.active_for_authentication?
+  end
+
+  test 'should not be active ' do
+    active = users(:disabled_user_account)
+    assert_not active.is_active?
+  end
+
+  test 'active_for_authentication? method should return false' do
+    active = users(:disabled_user_account)
+    assert_not active.active_for_authentication?
+  end
 end
