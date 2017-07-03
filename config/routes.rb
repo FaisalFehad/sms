a4a0e50b1db3c3fa2bb2b40b11af6c22016bc016
faisalfehad <faisalfehad@gmail.com>
2017-07-03 02:06:07 +0100

    Fix routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
-  root 'stocks#index'
-  devise_for :admins
-  devise_for :users
-  get '/history', to: 'orders#history'
-
-  get 'admin_panel/orders', as: 'all_orders'
-  get 'admin_panel/users', as: 'all_users'
-
-  post '/:user_id/', to: 'admin_panel#activation' , as: 'activation'
+  scope '/admin' do
+    get 'admin_panel/orders', to: 'admin_panel#orders', as: 'all_orders'
+    get 'admin_panel/users', to: 'admin_panel#users', as: 'all_users'
+    post ':user_id/', to: 'admin_panel#activation' , as: 'activation'
+  end
 
   resources :stocks do
     post '/:user/', to: 'orders#create', as: 'order'
   end
 
+  devise_for :admins
+  devise_for :users
+  get '/history', to: 'orders#history'
+  root 'stocks#index'
 end
