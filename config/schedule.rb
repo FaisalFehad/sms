17ff3f54f8b4b3a1cd882e9e66ff1076dd1b7e16
faisalfehad <faisalfehad@gmail.com>
2017-07-03 02:03:36 +0100

    Fix 2 days schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 # Disable user account if they orders wasn't returned in 2 days of the order
 every 6.hours do
   orders = orders.each do |order|
-    if order.fixed? && !order.returned && order.created_at > 2.day.ago
+    if order.fixed? && !order.returned && order.created_at > 2.day
       order.update_attribute(is_active:, false)
     end
   end
